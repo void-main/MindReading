9ba8c035934533247e8940cc5ea8bff541938f53
Void Main <voidmain1313113@gmail.com>
2013-03-05 16:33:14 +0800

    finish chrome extension

---
+++ frontend/background.js
@@ -0,0 +1,13 @@
+var putToCloud = function(url) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('PUT', 'http://mindreading.heroku.com/voidmain/test?url=' + url);
+  xmlhttp.send(null);
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  putToCloud(tab.url);
+});
+
+chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+  putToCloud(tag.url);
+});
