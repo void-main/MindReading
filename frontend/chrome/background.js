c992aac46ab987fa946d40f248ddffee162d78b8
Void Main <voidmain1313113@gmail.com>
2013-03-08 16:47:24 +0800

    update chrome browser extension

--- frontend/chrome/background.js
+++ frontend/chrome/background.js
@@ -1,13 +1,17 @@
 var putToCloud = function(url) {
+  if (url.substring(0, 4) != "http" && url.substring(0, 5) != "https") {
+    return;
+  }
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open('PUT', 'http://mindreading.heroku.com/voidmain/test?url=' + url);
   xmlhttp.send(null);
 };
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   putToCloud(tab.url);
 });
-
-chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-  putToCloud(tab.url);
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab) {
+    putToCloud(tab.url);
+  });
 });
